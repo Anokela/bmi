90127c3bd8a77f8ade03774a73117f3f4c00b662
Aleksi Nokela <n0noal00@student.oamk.fi>
2021-01-20 10:31:13 +0200

    Finished example.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
+
 import './App.css';
+import {useState}  from 'react'
 
 function App() {
+  const [height, setHeight] = useState(0);
+  const [weight, setWeight] = useState(0);
+  const [bmi, setBMI] = useState(0);
+  
+  function calculateBMI (e) {
+    e.preventDefault();
+    const calculation = weight / (height * height);
+    setBMI(calculation)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{padding: 30}}>
+      <h3>Calculating Body mass index</h3>
+      <form onSubmit= {calculateBMI}>
+        <div>
+          <label>Height: </label>
+          <input type="number" value={height} onChange={e => setHeight(e.target.value)}></input>
+        </div>
+        <div>
+          <label>Weight: </label>
+          <input type="number" value={weight} onChange={e => setWeight(e.target.value)}></input>
+        </div>
+        <div>
+          <label>BMI</label>
+          <output>{bmi.toFixed(1)}</output>
+        </div>
+        <div><button type="submit">Calculate</button></div>
+        {/* Button on oletruksen tyyppiä submit */}
+        
+      </form>
     </div>
   );
 }
